d8810e063c747367dadf441383013a16f3d6da2f
abr1043 <aojo4787@gmail.com>
2025-07-11 11:10:53 -0700

    add simple_interest.py to calculate simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# define the imput values 
+principal = 1000
+rate = o.o5
+time = 3
+
+# calculate simple interest
+interest = principal * rate * time
+
+# print the result
+print(f"the simple interest is: {interest}")
